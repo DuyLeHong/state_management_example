45f7e99f7af5bb9efa9b95a23ee8cf3af43172e1
DuyLeHong <leduy.av22@gmail.com>
2022-03-01 16:06:55 +0700

    Custom Provider: listen:true

--- lib/provider_ex.dart
+++ lib/provider_ex.dart
@@ -8,51 +8,61 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<MyModel>(
-      create: (context) {
-        return MyModel();
-      },
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: Text('My App')),
-          backgroundColor: Colors.grey,
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(20),
-                color: Colors.green[200],
-                child: Builder(
-                  builder: (BuildContext context) {
-                    return RaisedButton(
-                      child: Text('Do something 123'),
-                      onPressed: () {
-                        Provider.of<MyModel>(context, listen: false)
-                            .doSomething();
-                      },
-                    );
-                  },
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(35),
-                color: Colors.blue[200],
-                child: Consumer<MyModel>(
-                  builder: (context, mymodel, child) {
-                    return Text(mymodel.text);
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
+
+    MyModel appData = MyModel();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => appData),
+      ],
+      child: const MaterialApp(
+        home: MyScreen(),
       ),
     );
   }
 }
 
-class MyModel with ChangeNotifier {
+class MyScreen extends StatelessWidget{
+  const MyScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    MyModel data = Provider.of<MyModel>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('My App')),
+      backgroundColor: Colors.grey,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.green[200],
+            child: Builder(
+              builder: (BuildContext context) {
+
+                return RaisedButton(
+                  child: Text('Do something 123'),
+                  onPressed: () {
+                    data.doSomething();
+                  },
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(35),
+            color: Colors.blue[200],
+            child: Text(data.text),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyModel extends ChangeNotifier {
   String text = "Hello";
 
   void doSomething() {
